acb6227334e49c9d8e8bb813e5f309bb7edbb769
nimesh012 <cnimesh1412@gmail.com>
2023-06-10 15:40:15 +0530

    added

--- Book_worm/Book_worm/urls.py
+++ Book_worm/Book_worm/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name="index"),
+    path('top/', views.top, name="top"),
+    path('recommend/', views.recommend, name="recommend"),
+    path('recommendation/', views.recommendation, name="recommendation"),
 ]
+
