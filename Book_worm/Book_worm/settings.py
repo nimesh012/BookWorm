bd67ad8ec4bbca68058971860a50b8c926c99e79
nimesh012 <cnimesh1412@gmail.com>
2023-06-10 17:58:31 +0530

    updated

--- Book_worm/Book_worm/settings.py
+++ Book_worm/Book_worm/settings.py
@@ -71,6 +71,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'Book_worm.wsgi.application'
 
 
+
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
--- Book_worm/Book_worm/views.py
+++ Book_worm/Book_worm/views.py
@@ -6,10 +6,10 @@ import numpy as np
 import requests
 from random import sample
 
-popular_df = pd.read_csv('Book_worm\\popular.csv')
-books = pd.read_csv('Book_worm\\books.csv')
+popular_df = pd.read_csv('Book_worm\popular.csv')
+books = pd.read_csv('Book_worm\Books.csv')
 cs = pickle.load(open('cs.pkl', 'rb'))
-pt = pd.read_csv('Book_worm\\pt.csv')
+pt = pd.read_csv('Book_worm\pt.csv')
 
 
 def popular(df):
